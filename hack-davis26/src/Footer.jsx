b586f4f349be3cf992e82f870154bb00525d0ae8
MagicBattle <126199569+MagicBattle@users.noreply.github.com>
2026-05-09 16:46:17 -0700

    changed colors

--- hack-davis26/src/Footer.jsx
+++ hack-davis26/src/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer (){
     return(
-        <footer className="flex items-center justify-center px-6 py-4 border-t border-gray-200 mt-8">
-            <p className="text-sm text-gray-500">© 2026 Flipit</p>
+        <footer className="flex items-center justify-center px-6 py-4 bg-indigo-300 mt-8">
+            <p className="text-sm text-white">© 2026 Flipit</p>
         </footer>
     );
 }
--- hack-davis26/src/Header.jsx
+++ hack-davis26/src/Header.jsx
@@ -2,9 +2,9 @@ import { ScanHeart } from "lucide-react"
 
 function Header () {
     return(
-        <header className="flex items-center justify-between px-6 py-4 border-b border-gray-200">
-            <h1 className="text-2xl font-bold text-indigo-900">Flipit</h1>
-            <ScanHeart size={30} color="indigo"/>
+        <header className="flex items-center justify-between px-6 py-4 bg-indigo-300 shadow-md">
+            <h1 className="text-2xl font-bold text-white">Flipit</h1>
+            <ScanHeart size={30} color="white"/>
         </header>
     );
 }
